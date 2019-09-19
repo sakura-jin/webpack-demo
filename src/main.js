2c95b37c41780f2644b581b46c868104f5c716c5
sakura <929094868@qq.com>
2019-09-19 11:31:20 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import methods from './js/methods'
+
+console.log(methods.add(10,20));
+console.log(methods.mul(10,30));
+document.writeln('<p>路鸣泽</p>');
+require('./css/normal.css');
+require('./css/normal.scss');
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    entry:"./src/main.js",
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename:'bundle.js'
+    },
+    module: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: [ 'style-loader', 'css-loader' ]
+          },
+          {
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader" // 将 JS 字符串生成为 style 节点
+            }, {
+                loader: "css-loader" // 将 CSS 转化成 CommonJS 模块
+            }, {
+                loader: "sass-loader" // 将 Sass 编译成 CSS
+            }]
+          }
+        ]
+      }
+}
